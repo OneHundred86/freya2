e426448fd588d372ac1ebd753ffc593ce9e75617
chenguangjun <chen.gj@qq.com>
2018-07-09 14:56:51 +0800

    404

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -1,2 +1,49 @@
-自定义404</br>
-你请求的页面不存在
+<!DOCTYPE html>
+<html>
+<head>
+  <title>找不到该页面 404</title>
+  <style>
+    body {
+      color: #666;
+      text-align: center;
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+      margin: auto;
+      font-size: 14px;
+    }
+
+    h1 {
+      font-size: 56px;
+      line-height: 100px;
+      font-weight: 400;
+      color: #456;
+    }
+
+    .container {
+      margin: auto 20px;
+    }
+
+    .go-back {
+      display: none;
+    }
+
+  </style>
+</head>
+
+<body>
+  <h1>
+    404 页面不存在
+  </h1>
+  <div class="container">
+    <a href="javascript:history.back()" class="js-go-back go-back">返回</a>
+  </div>
+  <script>
+    (function () {
+      var goBack = document.querySelector('.js-go-back');
+
+      if (history.length > 1) {
+        goBack.style.display = 'inline';
+      }
+    })();
+  </script>
+</body>
+</html>
