f72ec37b865a30c44aa52dc4d1c84ce75264bc0e
chenguangjun <chen.gj@qq.com>
2018-07-10 14:57:54 +0800

    fix

--- app/Traits/Response.php
+++ app/Traits/Response.php
@@ -19,14 +19,18 @@ trait Response{
   public function view($view = null, $data = [], $mergeData = []) {
     $data['app_url'] = env('APP_URL');
     $data['version'] = env('VERSION');
-    $data['title'] = env('APP_TITLE', 'Freya2.0');
+    if(empty($data['title']))
+      $data['title'] = env('APP_TITLE', 'Freya2.0');
+
     return view($view, $data, $mergeData);
   }
 
   public function simpleView($view = null, $data = [], $statusCode = 200){
     $data['app_url'] = env('APP_URL');
     $data['version'] = env('VERSION');
-    $data['title'] = env('APP_TITLE', 'Freya2.0');
+    if(empty($data['title']))
+      $data['title'] = env('APP_TITLE', 'Freya2.0');
+    
     return response()->view($view, $data, $statusCode);
   }
 
