4c42db23997c4378d6c8bd1482627889efabbb4f
谢福成 <420006782@qq.com>
2018-07-10 14:14:27 +0800

    no message

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -1,50 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-  
-  <title>{{ $title }}</title>
-  <style>
-    body {
-      color: #666;
-      text-align: center;
-      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-      margin: auto;
-      font-size: 14px;
-    }
 
-    h1 {
-      font-size: 56px;
-      line-height: 100px;
-      font-weight: 400;
-      color: #456;
-    }
+@extends('error/base')
 
-    .container {
-      margin: auto 20px;
-    }
+@section('app')
+    <div class="error-frame">
+        <div class="msg-frame">
+            页面不存在
+        </div>
+        <div class="error-bottom">
+            <a class="link a" href="/">首页</a>
+            <a class="link" href="javascript:history.go(-1)" id="back">返回</a>
+        </div>
+    </div>
+    <script>
+        if(history.length <= 1){
+            var back = document.getElementById('back');
+            back.innerHTML = '关闭';
+            back.href = 'javascript:window.close()';
+        }
+    </script>
+@endsection
 
-    .go-back {
-      display: none;
-    }
-
-  </style>
-</head>
-
-<body>
-  <h1>
-    404 页面不存在
-  </h1>
-  <div class="container">
-    <a href="javascript:history.back()" class="js-go-back go-back">返回</a>
-  </div>
-  <script>
-    (function () {
-      var goBack = document.querySelector('.js-go-back');
-
-      if (history.length > 1) {
-        goBack.style.display = 'inline';
-      }
-    })();
-  </script>
-</body>
-</html>
--- resources/views/error/not_allowed.blade.php
+++ resources/views/error/not_allowed.blade.php
@@ -1,1 +1,22 @@
-权限不足，不允许访问
+
+@extends('error/base')
+
+@section('app')
+    <div class="error-frame">
+        <div class="msg-frame">
+            权限不足， 不允许访问
+        </div>
+        <div class="error-bottom">
+            <a class="link a" href="/">首页</a>
+            <a class="link" href="javascript:history.go(-1)" id="back">返回</a>
+        </div>
+    </div>
+    <script>
+        if(history.length <= 1){
+            var back = document.getElementById('back');
+            back.innerHTML = '关闭';
+            back.href = 'javascript:window.close()';
+        }
+    </script>
+@endsection
+
