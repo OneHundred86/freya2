794231bb4438cbdadeb5aa778d4f98714b70fa14
chenguangjun <chen.gj@qq.com>
2018-12-20 17:11:14 +0800

    fix

--- app/Traits/Response.php
+++ app/Traits/Response.php
@@ -27,23 +27,24 @@ trait Response{
 
   public function view($view = null, $data = [], $mergeData = []) {
     $data['app_url'] = env('APP_URL');
+    $data['version'] = env('VERSION');
+    if(empty($data['title']))
+      $data['title'] = env('APP_TITLE', 'Freya2.0');
+
     return view($view, $data, $mergeData);
   }
 
   public function simpleView($view = null, $data = [], $statusCode = 200){
     $data['app_url'] = env('APP_URL');
+    $data['version'] = env('VERSION');
+    if(empty($data['title']))
+      $data['title'] = env('APP_TITLE', 'Freya2.0');
+    
     return response()->view($view, $data, $statusCode);
   }
 
-  // 由路由名生成uri
-  public function route($routeName, $params = [], $absolute = true){
-    return route($routeName, $params, $absolute);
-  }
-
-  // 重定向到路由名
   public function redirect($routeName, $params = [], $absolute = true){
-    $uri = $this->route($routeName, $params, $absolute);
-
+    $uri = route($routeName, $params, $absolute);
     return redirect($uri);
   }
 
